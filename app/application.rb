cc1021e68f3858a8a06123d13d98f3f6aa2104f3
hkofkin <hkofkin@gmail.com>
2020-06-02 13:14:40 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,23 @@
+class Application
+
+    def call(env)
+
+        resp = Rack::Response.new
+        req = Rack::Request.new(env)
+
+        if req.path.match(/items/)
+            item_name = req.path.split("/items/").last
+            if item = @@items.find { |i| i.name == item_name }
+                resp.write item.price
+            else
+                resp.write "Item not found"
+                resp.status = 400
+            end
+        else
+            resp.write "Route not found"
+            resp.status = 404
+        end
+        resp.finish
+    end
+
+end
