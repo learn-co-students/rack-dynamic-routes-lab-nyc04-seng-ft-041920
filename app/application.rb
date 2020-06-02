eeb2f32b4d38dfba9efdb538ed5a211929f348fc
frod25 <frod25>
2020-06-01 20:13:03 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,26 @@
+class Application
+
+    @@items = []
+
+    def call(env)
+        resp = Rack::Response.new
+        req = Rack::Request.new(env)
+
+        
+        if req.path.match(/items/)
+            item_name = req.path.split("/items/").last
+            found_item = @@items.find {|i| i.name == item_name }
+            if found_item
+                resp.write(found_item.price)
+            else
+                resp.write("Item not found")
+                resp.status = 400
+            end
+        else
+            resp.write("Route not found")
+            resp.status = 404
+        end
+
+        resp.finish
+    end
+end
